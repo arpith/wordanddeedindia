6e2f9b4a207bf8633f768c211e88105ecfbad82c
Arpith Siromoney <arpith@feedreader.co>
2016-03-15 12:27:54 +0530

    just display header image for now

--- components/App.js
+++ components/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import Header from './Header';
 import HeaderImg from './HeaderImg';
  
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
         <HeaderImg />
-        <Headline />
-        <Need />
-        <Donation />
-        <You />
-        <Impact />
       </div>
     );
   }
