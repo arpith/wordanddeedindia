251a725431266ee6dc2e05a6fd085962f2dbff75
Arpith Siromoney <arpith@feedreader.co>
2016-03-15 12:44:14 +0530

    misc fixes

--- components/HeaderImg.js
+++ components/HeaderImg.js
@@ -3,8 +3,8 @@ import React from 'react';
 class HeaderImg extends React.Component {
   render() {
     return (
-      <div class="header-img">
-        <img class="imgix-fluid" data-src="//assets.imgix.net/examples/octopus.jpg?fit=crop&crop=faces" >
+      <div className="header-img">
+        <img className="imgix-fluid" data-src="//assets.imgix.net/examples/octopus.jpg?fit=crop&crop=faces" />
       </div>
     );
   }
