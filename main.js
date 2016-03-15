53e4d48086d4752f960141869b2554d048a906b3
Arpith Siromoney <arpith@feedreader.co>
2016-03-15 12:31:23 +0530

    add imgix fluid code to main js

--- main.js
+++ main.js
@@ -3,6 +3,27 @@ import { render } from 'react-dom';
 import { browserHistory, Router } from 'react-router';
 import routes from './routes';
 
+const options = {
+  updateOnResizeDown : true,
+  updateOnPinchZoom : true,
+  fitImgTagToContainerWidth: true,
+  fitImgTagToContainerHeight: true,
+  pixelStep : 10,
+  onChangeParamOverride: function(w, h) {
+    var dpr = Math.ceil(window.devicePixelRatio*10) /10;
+    return {
+      "txt": "w:" + w + " h:" +h + " dpr:" + dpr,
+      "txtalign": "center,bottom",
+      "txtsize": 20,
+      "txtfont": "Helvetica%20Neue,bold",
+      "txtclr": "ffffffff",
+      "txtpad": 20,
+      "txtfit": 'max',
+      "exp": -2
+    }
+  }
+};
+
 let mountNode = document.getElementById("react-mount");
 let router = (
   <Router 
@@ -12,3 +33,7 @@ let router = (
 );
 
 render(router, mountNode);
+
+imgix.onready(function() {
+  imgix.fluid(options);
+});
