a723d6f0a6267063205c713efa11eb4084f180e0
Arpith Siromoney <arpith@feedreader.co>
2016-03-18 13:34:28 +0530

    Update App.js

--- components/App.js
+++ components/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './Header';
 import HeaderImg from './HeaderImg';
- 
+import Headline from './Headline';
+
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
         <HeaderImg />
+        <Header />
+        <Headline />
       </div>
     );
   }
