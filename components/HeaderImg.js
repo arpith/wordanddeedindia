4a57554050ef2e00188d6f952a8f7eb3891e710f
Arpith Siromoney <arpith@feedreader.co>
2016-03-18 11:55:50 +0530

    Add inline style for header image component

--- components/HeaderImg.js
+++ components/HeaderImg.js
@@ -2,9 +2,20 @@ import React from 'react';
  
 class HeaderImg extends React.Component {
   render() {
+    const style = {
+      width: '98%',
+      height: '500px',
+      clear: 'both',
+      margin: '0 1% .5em',
+      text-align: 'center',
+      overflow: 'hidden'
+    };
+    
+    const src = 'https://wordanddeedindia.imgix.net/images/child.jpg?fit=crop&crop=faces';
+
     return (
-      <div className="header-img">
-        <img className="imgix-fluid" data-src="https://wordanddeedindia.imgix.net/images/child.jpg?fit=crop&crop=faces" />
+      <div style={style}>
+        <img className="imgix-fluid" data-src={src} />
       </div>
     );
   }
