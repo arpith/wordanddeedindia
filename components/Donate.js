acd7d705b593c0021a9319b0ed8b98facd1e0eab
Arpith Siromoney <arpith@feedreader.co>
2016-03-18 12:59:35 +0530

    Create Donate.js

---
+++ components/Donate.js
@@ -0,0 +1,16 @@
+import React from 'react';
+ 
+class Donate extends React.Component {
+  render() {
+    const style = {
+      backgroundColor: '#50B5A6'
+    };
+    
+    return (
+      <div style={style}>
+      </div>
+    );
+  }
+}
+
+export default Donate;
