3a78b371cb69bf85f6da47c1c546988478a51581
Arpith Siromoney <arpith@feedreader.co>
2016-03-18 11:48:23 +0530

    Use child image in header image component

--- components/HeaderImg.js
+++ components/HeaderImg.js
@@ -4,7 +4,7 @@ class HeaderImg extends React.Component {
   render() {
     return (
       <div className="header-img">
-        <img className="imgix-fluid" data-src="//assets.imgix.net/examples/octopus.jpg?fit=crop&crop=faces" />
+        <img className="imgix-fluid" data-src="https://wordanddeedindia.imgix.net/images/child.jpg?fit=crop&crop=faces" />
       </div>
     );
   }
