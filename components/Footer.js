16367e2520490d79b05439e9450253108e8e024f
Arpith Siromoney <arpith@feedreader.co>
2016-03-18 13:07:02 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+ 
+class Footer extends React.Component {
+  render() {
+    const style = {
+      backgroundColor: '#E55B6B'
+    };
+    
+    return (
+      <div style={style}>
+      </div>
+    );
+  }
+}
+
+export default Footer;
