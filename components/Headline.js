8a0dae12ffedf19412a55d3a02315217965fddf1
Arpith Siromoney <arpith@feedreader.co>
2016-03-18 13:33:30 +0530

    Create Headline.js

---
+++ components/Headline.js
@@ -0,0 +1,22 @@
+import React from 'react';
+ 
+class Headline extends React.Component {
+  render() {
+    const style = {
+      width: '40%',
+      marginLeft: '60%',
+      clear: 'both',
+      float: 'left',
+      color: 'white'
+    };
+    
+    return (
+      <div style={style}>
+        <h1>Educate a child</h1>
+        <div>The gift of education can break the cycle of poverty and prevent child labour.</div>
+      </div>
+    );
+  }
+}
+
+export default Headline;
